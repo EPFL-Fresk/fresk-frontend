7f1e0a68a6bf545319671249a418be4ee8fb108a
Camille Lannoye <101193378+camillelnne@users.noreply.github.com>
2024-11-30 20:27:55 +0100

    refactor(nde ui): extract logo widget

--- lib/widget/node.dart
+++ lib/widget/node.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 /// A round widget that contains an image.
 class Node extends StatefulWidget {
-  const Node({super.key, required this.image});
+  const Node({super.key, required this.name, required this.image});
 
+  final String name;
   final String image;
 
   @override
@@ -26,9 +27,9 @@ class _NodeState extends State<Node> {
         width: selected ? avatarDiameter * 3 : avatarDiameter,
         height: selected ? avatarDiameter * 2 : avatarDiameter,
         duration: const Duration(milliseconds: 500),
-        curve: Curves.easeInOut, // Optional: Adds a smooth curve to the animation
+        curve: Curves.easeInOut,
         decoration: BoxDecoration(
-          color: selected ? Colors.blue.withOpacity(0.2) : Colors.transparent,
+          color: Colors.blue.withOpacity(0.2),
           border: Border.all(color: Colors.white, width: 6.0),
           borderRadius:
               BorderRadius.circular(selected ? 16 : avatarDiameter / 2),
@@ -38,20 +39,37 @@ class _NodeState extends State<Node> {
               BorderRadius.circular(selected ? 16 : avatarDiameter / 2),
           child: Stack(
             children: [
-              // Other widgets can be added here when expanded
-              AnimatedPositioned(
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.easeInOut,
-                top: selected ? 6.0 : (avatarDiameter / 2) - (avatarDiameter / 2),
-                left: selected ? 6.0 : (avatarDiameter / 2) - (avatarDiameter / 2),
-                child: CircleAvatar(
-                  radius: avatarDiameter / 2,
-                  backgroundImage: NetworkImage(widget.image),
-                ),
-              ),
+              Logo(selected: selected, avatarDiameter: avatarDiameter, widget: widget),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Logo extends StatelessWidget {
+  const Logo({
+    super.key,
+    required this.selected,
+    required this.avatarDiameter,
+    required this.widget,
+  });
+
+  final bool selected;
+  final double avatarDiameter;
+  final Node widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+      top: selected ? 6.0 : (avatarDiameter / 2) - (avatarDiameter / 2),
+      left: selected ? 6.0 : (avatarDiameter / 2) - (avatarDiameter / 2),
+      child: CircleAvatar(
+        radius: selected ? avatarDiameter / 4 : avatarDiameter / 2,
+        backgroundImage: NetworkImage(widget.image),
       ),
     );
   }
