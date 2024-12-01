6342d3346c763ff4f39250e1d130433bbed5db8a
--replace-all <muchembled.martin@gmail.com>
2024-12-01 11:57:36 +0100

    feat: Made MainApp statefull #16

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import "package:flutter/material.dart";
+import "package:fresk/model/association.dart";
 import "package:fresk/screen/HomeScreen.dart";
+import "package:fresk/utils/data.dart";
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MainAppState();
+  }
+}
+
+class MainAppState extends State<MainApp> {
+  late Future<List<Association>> associations;
+
+  @override
+  void initState() {
+    super.initState();
+
+    associations = fetchAssociations();
+  }
 
   @override
   Widget build(BuildContext context) {
